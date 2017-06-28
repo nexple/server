2fc36029bd438242d09becac132c3527d2c4996a
Marko Mäkelä <marko.makela@mariadb.com>
2017-06-28 16:47:21 +0300

    Mariabackup: Work around MDEV-13093

--- extra/mariabackup/xtrabackup.cc
+++ extra/mariabackup/xtrabackup.cc
@@ -2734,6 +2734,10 @@ xb_load_single_table_tablespace(
 
 	ut_free(name);
 
+	if (fil_space_crypt_t* crypt_info = file->get_crypt_info()) {
+		fil_space_destroy_crypt_data(&crypt_info);
+	}
+
 	delete file;
 
 	if (err != DB_SUCCESS && err != DB_CORRUPTION && xtrabackup_backup) {
